af6bd21fb5976e193d3829314f37d1f8534f2620
Nathanial Gardner <nathanial.gardner@gmail.com>
2012-11-18 09:49:41 -0500

    fixing bug in seed logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ WellpointUser.delete_all
 
 eligibility_updates = []
 csv_data = File.open(Rails.root.join("wp_user_mock_data.csv"))
-csv_data.gsub("| |", "||")
+csv_data.read.gsub!("| |", "||")
 CSV.foreach(csv_data,  :col_sep => "|",  :quote_char => '"',  :headers =>  :first_row,  :return_headers => false) do |row|
   values = {}
   [ 
