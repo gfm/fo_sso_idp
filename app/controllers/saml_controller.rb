d0179cd1850a087317936bb7a7e32dd6bbed06e0
Nathanial Gardner <nathanial.gardner@gmail.com>
2012-11-19 09:07:30 -0500

    changing issuer for mfo-qa1

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -41,27 +41,6 @@ class SamlController < ApplicationController
 
       settings.idp_sso_target_url = "#{request.protocol}#{request.host_with_port}/saml/auth?issuer=#{settings.issuer}#{id.nil? ? '' : '&id='+id.to_s}"
       
-      # # local consume test
-      # # settings.assertion_consumer_service_url  = "http://localhost:3000/saml/consume"
-      # # settings.issuer                         = "http://localhost:3000"
-
-      # # fitorbit.dev consume test
-      # # settings.assertion_consumer_service_url = "http://fitorbit.dev/saml/consume"
-      # # settings.issuer                         = "http://fitorbit.dev"
-
-      # # qa.myfitorbit.com consume test
-      # settings.assertion_consumer_service_url = "https://qa1.myfitorbit.com/saml/consume"
-      # settings.issuer                         = "saml.uat.anthem.com:FitOrbit"
-      
-      # # #Sample Saml Response
-      # # settings.name_identifier_format         = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
-      # # settings.idp_cert_fingerprint           = "9E:65:2E:03:06:8D:80:F2:86:C7:6C:77:A1:D9:14:97:0A:4D:F4:4D"
-      
-      # # #WP Saml Response
-      # settings.name_identifier_format         = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
-      # settings.idp_cert_fingerprint           = "05:AF:2D:72:4F:0D:5E:E9:2D:49:E8:4F:DC:19:5D:6C:43:67:25:47"
-      # settings.private_key                    = "/Users/nate/Downloads/qa.myfitorbit.key"
-
       settings
     end
   end
